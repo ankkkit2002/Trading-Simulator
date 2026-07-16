d604835d741b42975d01b288dc71eda2caef8003
Ankit <akankit7050@gmail.com>
2026-07-16 19:54:49 +0530

    Add title,profile button and navigation bar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,17 +4,32 @@ import "./NavBar.css"
 function Navigation() {
     return (
         <div>
-            <div>
-                <header>
-                    <nav className="navigation_bar">
-                        <NavLink to="/dashboard" style={{ color: "#4d4c4c" ,textDecoration: "none"}} >Dashboard</NavLink>
-                        <NavLink to="/holdings" style={{ color: "#4d4c4c",textDecoration: "none"}}>Holdings</NavLink>
-                        <NavLink to="/positions" style={{ color: "#4d4c4c" ,textDecoration: "none" }}>Positions</NavLink>
-                        <NavLink to="/order" style={{ color: "#4d4c4c" ,textDecoration: "none" }}>Order</NavLink>
-                        <NavLink to="/funds" style={{ color: "#4d4c4c" ,textDecoration: "none"}}>Funds</NavLink>
+
+            <header className="navigation_bar">
+               <h1 id="heading">Trading Simulator </h1>
+                <div>
+                     
+                    <nav className="nav1" >
+                        <NavLink to="/dashboard" style={{ color: "#4d4c4c", textDecoration: "none" }} >Dashboard</NavLink>
+                        <NavLink to="/holdings" style={{ color: "#4d4c4c", textDecoration: "none" }}>Holdings</NavLink>
+                        <NavLink to="/positions" style={{ color: "#4d4c4c", textDecoration: "none" }}>Positions</NavLink>
+                        <NavLink to="/order" style={{ color: "#4d4c4c", textDecoration: "none" }}>Order</NavLink>
+                        <NavLink to="/funds" style={{ color: "#4d4c4c", textDecoration: "none" }}>Funds</NavLink>
+
                     </nav>
-                </header>
-            </div>
+                </div>
+
+                <div id="profile">
+                    <button id="but_profile" type="button">
+                        👤 <span id="user_name">Ankit</span>
+                    </button>
+                     
+
+                </div>
+
+            </header>
+
+            <hr id="first_hr" />
         </div>
     )
 }
