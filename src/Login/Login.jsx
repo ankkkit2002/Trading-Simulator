bc76667dae40eb8abc8efb3d0be737b74f703da5
Ankit <akankit7050@gmail.com>
2026-08-12 23:09:03 +0530

    change UI of login page

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -12,19 +12,28 @@ function Login() {
     }
     return (
         <div className={styles.main}>
-            <div className={styles.heading}>
-                <h1>Trading Simulator</h1>
+            <div className={styles.left_side}>
+                <h1>Trading simulator</h1>
+                <p className={styles.statement}>Full Real Trading Experience</p>
+                <p className={styles.statement}>Ultra Fast Execution </p>
+                <p className={styles.statement}>Live Charts with BUY and SELL</p>
             </div>
-            <div className={styles.box}>
-                <input type="text" placeholder="Username" className={styles.input} />
-                <input type="text" placeholder="Password" className={styles.input2} />
-                <div onClick={handleLogin} className={styles.login} >
-                    <button>Login</button>
+            <div className={styles.container}>
+
+                <div className={styles.box}>
+                    <div className={styles.heading}>
+                        <h2>Trading Simulator</h2>
+                    </div>
+                    <input type="text" placeholder="Username" className={styles.input} />
+                    <input type="text" placeholder="Password" className={styles.input2} />
+                    <div onClick={handleLogin} className={styles.login} >
+                        <button>Login</button>
+                    </div>
+                    <p className={styles.account}>Don't have an account? Sign up</p>
                 </div>
-                <p className={styles.account}>Don't have an account? Sign up</p>
+
+
             </div>
-
-
         </div>
     )
 }
